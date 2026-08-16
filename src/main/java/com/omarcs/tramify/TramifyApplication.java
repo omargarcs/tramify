dae9527f2ce5293598439204e21a7561aedab26c
Omar García <omardario95@hotmail.com>
2026-08-15 21:51:57 -0600

    Initial commit: base Spring Boot project structure

---
+++ src/main/java/com/omarcs/tramify/TramifyApplication.java
@@ -0,0 +1,13 @@
+package com.omarcs.tramify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omarcs/tramify/TramifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarcs.tramify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

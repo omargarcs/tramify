db767d47a4eb2f63b2330904f2c1c588f6ecd9da
Omar García <omardario95@hotmail.com>
2026-09-06 14:43:38 -0600

    Refactor ExpiringDocumentDto to a record

--- src/main/java/com/omarcs/tramify/dto/ExpiringDocumentDto.java
+++ src/main/java/com/omarcs/tramify/dto/ExpiringDocumentDto.java
@@ -1,17 +1,8 @@
 package com.omarcs.tramify.dto;
 
 import com.omarcs.tramify.dto.enums.DocumentOrigin;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
-@Getter
-public class ExpiringDocumentDto {
-
-    private final String personName;
-    private final String documentType;
-    private final LocalDate expiryDate;
-    private final DocumentOrigin documentOrigin;
-}
+public record ExpiringDocumentDto (String personName, String documentType, LocalDate expiryDate,
+    DocumentOrigin documentOrigin){}

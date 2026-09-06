5cc828a001044b897a67839d66f906d758238ae9
Omar García <omardario95@hotmail.com>
2026-09-06 14:37:58 -0600

    Refactor ExpiringDocumentDto to use Lombok annotations

--- src/main/java/com/omarcs/tramify/dto/ExpiringDocumentDto.java
+++ src/main/java/com/omarcs/tramify/dto/ExpiringDocumentDto.java
@@ -1,36 +1,17 @@
 package com.omarcs.tramify.dto;
 
 import com.omarcs.tramify.dto.enums.DocumentOrigin;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@Getter
 public class ExpiringDocumentDto {
 
     private final String personName;
     private final String documentType;
     private final LocalDate expiryDate;
     private final DocumentOrigin documentOrigin;
-
-    public ExpiringDocumentDto (String personName, String documentType, LocalDate expiryDate, DocumentOrigin documentOrigin){
-        this.personName = personName;
-        this.documentType = documentType;
-        this.expiryDate = expiryDate;
-        this.documentOrigin = documentOrigin;
-    }
-
-    public String getPersonName() {
-        return personName;
-    }
-
-    public String getDocumentType() {
-        return documentType;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public DocumentOrigin getDocumentOrigin() {
-        return documentOrigin;
-    }
 }
